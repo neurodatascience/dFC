a3fc6fa93404eb5dd545a574c858223f86a0d803
mtorabi59 <mohammad.torabi@mail.mcgill.ca>
2026-06-03 16:22:09 -0400

    make dFC_mat_lst_similarity faster

--- pydfc/comparison/similarity_assessment.py
+++ pydfc/comparison/similarity_assessment.py
@@ -223,8 +223,26 @@ class SimilarityAssessment:
         return feature
 
     def dFC_mat_lst_similarity(
-        self, dFC_mat_lst, feature2extract, metric, graph_property=None
+        self, dFC_mat_lst, feature2extract, metric, graph_property=None, precompute=False
     ):
+        if precompute:
+            # pre-compute features once per matrix (avoids O(n^2) recomputation)
+            features = [
+                self.extract_feature(
+                    dFC_mat,
+                    feature2extract=feature2extract,
+                    graph_property=graph_property,
+                )
+                for dFC_mat in dFC_mat_lst
+            ]
+            # pre-rank for spearman: Pearson(rank(a), rank(b)) == spearmanr(a, b)
+            if metric == "spearman":
+                ranked_features = [
+                    np.apply_along_axis(
+                        lambda x: stats.rankdata(x, method="average"), 1, feat
+                    )
+                    for feat in features
+                ]
 
         sim_mat_over_sample = None
         for i, dFC_mat_i in enumerate(dFC_mat_lst):
@@ -235,16 +253,20 @@ class SimilarityAssessment:
 
                 assert dFC_mat_i.shape == dFC_mat_j.shape, "shape mismatch"
 
-                feature_i = self.extract_feature(
-                    dFC_mat_i,
-                    feature2extract=feature2extract,
-                    graph_property=graph_property,
-                )  # (samples, variables)
-                feature_j = self.extract_feature(
-                    dFC_mat_j,
-                    feature2extract=feature2extract,
-                    graph_property=graph_property,
-                )  # (samples, variables)
+                if precompute:
+                    feature_i = features[i]  # (samples, variables)
+                    feature_j = features[j]  # (samples, variables)
+                else:
+                    feature_i = self.extract_feature(
+                        dFC_mat_i,
+                        feature2extract=feature2extract,
+                        graph_property=graph_property,
+                    )  # (samples, variables)
+                    feature_j = self.extract_feature(
+                        dFC_mat_j,
+                        feature2extract=feature2extract,
+                        graph_property=graph_property,
+                    )  # (samples, variables)
 
                 sim_over_sample = list()
                 for sample in range(feature_i.shape[0]):
@@ -259,9 +281,15 @@ class SimilarityAssessment:
                                 0, 1
                             ]
                         elif metric == "spearman":
-                            sim, p = stats.spearmanr(
-                                feature_i[sample, :], feature_j[sample, :]
-                            )
+                            if precompute:
+                                sim = np.corrcoef(
+                                    ranked_features[i][sample, :],
+                                    ranked_features[j][sample, :],
+                                )[0, 1]
+                            else:
+                                sim, _ = stats.spearmanr(
+                                    feature_i[sample, :], feature_j[sample, :]
+                                )
                         elif metric == "MI":
                             sim = mutual_information(
                                 X=feature_i[sample, :], Y=feature_j[sample, :], N_bins=100
@@ -308,13 +336,6 @@ class SimilarityAssessment:
         methods_assess["TS_info_lst"] = TS_info_lst
         methods_assess["common_TRs"] = common_TRs
 
-        ########## dFC samples ##########
-
-        dFC_samples = {}
-        for i, dFC_mat in enumerate(dFC_mat_lst):
-            dFC_samples[str(i)] = dFC_mat
-        methods_assess["dFC_samples"] = dFC_samples
-
         ########## time record ##########
 
         time_record_dict = {}
@@ -332,7 +353,7 @@ class SimilarityAssessment:
         methods_assess["all"] = {}
         for metric in metric_list:
             methods_assess["all"][metric] = self.dFC_mat_lst_similarity(
-                dFC_mat_lst, feature2extract="all", metric=metric
+                dFC_mat_lst, feature2extract="all", metric=metric, precompute=True
             )
         ##############################################
         return methods_assess
